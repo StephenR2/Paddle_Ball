a817790eed2fe30607f114d2628af4ad51971dbf
StephenR2 <stephen@iMac2.local>
2018-05-03 08:26:19 -0400

    Character has been switched to Paddle, Ball bounces

---
+++ Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿/* Stephen Randall
+ * 03/16/18
+ * This script is responsible for position of paddle, speed of paddle, etc.
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player: MonoBehaviour {
+	public float paddleSpeed = .25f; // Sets paddle move speed
+	private Vector3 playerPos;
+	private float zPos = -9.5f;
+
+	void Start () {
+		transform.position = new Vector3 (0, 1, -9.5f);
+
+
+	}
+	// Update is calle
+	void Update () {
+		float xPos = transform.position.x + (Input.GetAxis ("Horizontal") * paddleSpeed); // Sets position and what axis it moves on
+		float yPos = transform.position.y + (Input.GetAxis ("Vertical") * paddleSpeed);
+		playerPos = new Vector3 (Mathf.Clamp(xPos, -5f, 5f),Mathf.Clamp(yPos,1f, 7.5f),-9.5f);
+		transform.position = playerPos; // Set ball to follow paddle
+	}
+}
---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,62 @@
+﻿/* Stephen Randall
+ * 03/16/18
+ * This script is responsible for ball, putting ball in play, etc.
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	public float ballInitialVelocity = 600f; // Sets balls velocity
+	private Rigidbody rb; 
+	private bool BallInPlay; // Flag for when the ball is in play.
+	private Vector3 DirectionPoint;
+	private Quaternion Rotating;
+
+
+	// Use this for initialization
+	void Awake () {
+		rb = GetComponent<Rigidbody> (); // Makes it a rigidbody
+
+
+	}
+	void FixedUpdate (){
+//		Debug.Log ("Enter Update");
+		if ((Input.touchCount > 0) && (Input.GetTouch (0).phase == TouchPhase.Began)) {
+//			Debug.Log ("Touched");
+			Ray raycast = Camera.main.ScreenPointToRay (Input.GetTouch (0).position);
+			RaycastHit raycastHit = new RaycastHit ();
+			if (Physics.Raycast (raycast, out raycastHit)) {
+//				raycastHit.point = DirectionPoint;
+//				transform.LookAt (raycastHit.point);
+//				rb.isKinematic = false;
+//				rb.AddRelativeForce (raycastHit.point * 100);
+				Serve (raycastHit.point);
+			}
+
+		}
+	}
+
+		
+
+	// Update is called once per frame
+		void Serve (Vector3 touchpos)
+	{
+		
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit)) {
+			Vector3 dir = touchpos - transform.position;
+			dir = dir.normalized; 
+			rb.isKinematic = false;
+			transform.LookAt (dir);
+			rb.AddRelativeForce (dir * 750);
+			//BallInPlay = true;
+			//rb.AddForce(new Vector3(-ballInitialVelocity, ballInitialVelocity, 0));
+			
+		}
+	}
+}
+
+
+	
